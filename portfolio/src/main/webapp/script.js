a5228c910030d52d95063466f7e7f713017fd046
shmcaffrey <45078226+shmcaffrey@users.noreply.github.com>
2020-06-30 13:54:42 -0400

    change argument in forEach and remove old code

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -73,57 +73,9 @@ function getComments() {
     fetch('/data').then(response => response.json()).then((comments) => {
         const commentList = document.getElementById("comment-container");
         console.log("fetching comments");
-        comments.forEach((comments) => {
-            commentList.appendChild(createListElement(comments));
+        comments.forEach((comment) => {
+            commentList.appendChild(createListElement(comment));
         })
     });
 }
 
-// For learning promises
-function getCommentsLong() {
-    console.log('fetching JSON');
-
-    const responsePromise = fetch('/data');
-
-    responsePromise.then(handleResponse);
-}
-
-function handleResponse(response) {
-    console.log('convert JSON promise stream to text');
-    const textPromise = response.json();
-    console.log(textPromise);
-    textPromise.then(addListToDom);
-}
-
-function addListToDom(list) {
-    const commentList = document.getElementById("comment-container");
-    commentList.innerText = '';
-    // list.pageOne does not work as a JSON element should, should this work with array format?
-    commentList.appendChild(
-      createListElement("Page One: " + list[0]));
-    commentList.appendChild(
-      createListElement("Page Two: " + list[1]));
-    commentList.appendChild(
-      createListElement("Page Three: " + list[2]));
-}
-
-/** Creates an <li> element containing text. */
-function createListElement(text) {
-  const liElement = document.createElement('li');
-  liElement.innerText = text;
-  return liElement;
-}
-
-async function getCommentsAsyncAwait() {
-    const response = await fetch('/data');
-    const comments = await response.json();
-    const commentList = document.getElementById("comment-container");
-    commentList.innerText = '';
-    commentList.appendChild(
-      createListElement("Page One: " + comments[0]));
-    commentList.appendChild(
-      createListElement("Page Two: " + comments[1]));
-    commentList.appendChild(
-      createListElement("Page Three: " + comments[2]));
-
-}
