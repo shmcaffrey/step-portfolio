1ae43dfd5916e1799eb4368fd7ad533017967edb
shmcaffrey <45078226+shmcaffrey@users.noreply.github.com>
2020-07-01 19:48:24 -0400

    Fix indentation dataServlet

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -83,9 +83,9 @@ public class DataServlet extends HttpServlet {
     response.sendRedirect("/index.html");
   }
 
-    private String convertJsonUsingGson(ArrayList<String> comments) {
-      String json = new Gson().toJson(comments);
-      return json;
+  private String convertJsonUsingGson(ArrayList<String> comments) {
+    String json = new Gson().toJson(comments);
+    return json;
   }
 
   private int getCommentInput(HttpServletRequest request) {
