042ad4123b45832caf186b506984bc9ef96b5137
shmcaffrey <45078226+shmcaffrey@users.noreply.github.com>
2020-07-09 09:08:03 -0400

    change \" to \' according to style guide

--- portfolio/src/main/webapp/scriptMaps.js
+++ portfolio/src/main/webapp/scriptMaps.js
@@ -14,85 +14,85 @@ function initMap() {
 
     var darkMode = new google.maps.StyledMapType(
         [
-            { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
-            { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
-            { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+            { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+            { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+            { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
             {
-                featureType: "administrative.locality",
-                elementType: "labels.text.fill",
-                stylers: [{ color: "#d59563" }]
+                featureType: 'administrative.locality',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#d59563' }]
             },
             {
-                featureType: "poi",
-                elementType: "labels.text.fill",
-                stylers: [{ color: "#d59563" }]
+                featureType: 'poi',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#d59563' }]
             },
             {
-                featureType: "poi.park",
-                elementType: "geometry",
-                stylers: [{ color: "#263c3f" }]
+                featureType: 'poi.park',
+                elementType: 'geometry',
+                stylers: [{ color: '#263c3f' }]
             },
             {
-                featureType: "poi.park",
-                elementType: "labels.text.fill",
-                stylers: [{ color: "#6b9a76" }]
+                featureType: 'poi.park',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#6b9a76' }]
             },
             {
-                featureType: "road",
-                elementType: "geometry",
-                stylers: [{ color: "#38414e" }]
+                featureType: 'road',
+                elementType: 'geometry',
+                stylers: [{ color: '#38414e' }]
             },
             {
-                featureType: "road",
-                elementType: "geometry.stroke",
-                stylers: [{ color: "#212a37" }]
+                featureType: 'road',
+                elementType: 'geometry.stroke',
+                stylers: [{ color: '#212a37' }]
             },
             {
-                featureType: "road",
-                elementType: "labels.text.fill",
-                stylers: [{ color: "#9ca5b3" }]
+                featureType: 'road',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#9ca5b3' }]
             },
             {
-                featureType: "road.highway",
-                elementType: "geometry",
-                stylers: [{ color: "#746855" }]
+                featureType: 'road.highway',
+                elementType: 'geometry',
+                stylers: [{ color: '#746855' }]
             },
             {
-                featureType: "road.highway",
-                elementType: "geometry.stroke",
-                stylers: [{ color: "#1f2835" }]
+                featureType: 'road.highway',
+                elementType: 'geometry.stroke',
+                stylers: [{ color: '#1f2835' }]
             },
             {
-                featureType: "road.highway",
-                elementType: "labels.text.fill",
-                stylers: [{ color: "#f3d19c" }]
+                featureType: 'road.highway',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#f3d19c' }]
             },
             {
-                featureType: "transit",
-                elementType: "geometry",
-                stylers: [{ color: "#2f3948" }]
+                featureType: 'transit',
+                elementType: 'geometry',
+                stylers: [{ color: '#2f3948' }]
             },
             {
-                featureType: "transit.station",
-                elementType: "labels.text.fill",
-                stylers: [{ color: "#d59563" }]
+                featureType: 'transit.station',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#d59563' }]
             },
             {
-                featureType: "water",
-                elementType: "geometry",
-                stylers: [{ color: "#17263c" }]
+                featureType: 'water',
+                elementType: 'geometry',
+                stylers: [{ color: '#17263c' }]
             },
             {
-                featureType: "water",
-                elementType: "labels.text.fill",
-                stylers: [{ color: "#515c6d" }]
+                featureType: 'water',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#515c6d' }]
             },
             {
-                featureType: "water",
-                elementType: "labels.text.stroke",
-                stylers: [{ color: "#17263c" }]
+                featureType: 'water',
+                elementType: 'labels.text.stroke',
+                stylers: [{ color: '#17263c' }]
             }
-        ], { name: "Dark Mode" });
+        ], { name: 'Dark Mode' });
 
     var chelseaLatLng = { lat: 42.318, lng: -84.020 };
     var hagueLatLng = { lat: 52.0705, lng: 4.3007 };
@@ -119,27 +119,27 @@ function initMap() {
         "</div></div>";
 
     var map = new google.maps.Map(
-        document.getElementById("map"), {
+        document.getElementById('map'), {
         center: chelseaLatLng,
         zoom: 8,
-        gestureHandling: "cooperative",
+        gestureHandling: 'cooperative',
         mapTypeControlOptions: {
-            mapTypeIds: ["roadmap", "satellite", "hybrid", "terrain",
-                "dark_mode"]
+            mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+                'dark_mode']
         }
     });
 
     cMarker = new google.maps.Marker({
         position: chelseaLatLng,
         map: map,
-        title: "Hometown",
+        title: 'Hometown',
         animation: google.maps.Animation.DROP,
     });
 
     hMarker = new google.maps.Marker({
         position: hagueLatLng,
         map: map,
-        title: "Au Pair Town",
+        title: 'Au Pair Town',
         animation: google.maps.Animation.DROP,
     });
 
@@ -153,14 +153,14 @@ function initMap() {
         pixelOffset:  new google.maps.Size(0, -20)
     });
 
-    cMarker.addListener("click", function() {
+    cMarker.addListener('click', function() {
         bounce(cMarker, chelseaContent);
     });
-    hMarker.addListener("click", function() {
+    hMarker.addListener('click', function() {
         bounce(hMarker, hagueContent);
     });
-    map.mapTypes.set("dark_mode", darkMode);
-    map.mapTypeId("dark_mode");
+    map.mapTypes.set('dark_mode', darkMode);
+    map.mapTypeId('dark_mode');
     map.setTilt(45);
 }
 
